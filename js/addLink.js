755432e10038e403214f706e352a4a42a71d488b
Ethan Perez <ethan@ethanperez.com>
2015-03-25 22:27:07 +0000

    Pre clean

--- js/addLink.js
+++ js/addLink.js
@@ -1,23 +1,33 @@
+String.prototype.capitalize = function(){
+  return this.replace( /(^|\s)([a-z])/g , function(m,p1,p2){ return p1+p2.toUpperCase(); } );
+};
 
 // jQuery function to strip search string of args
 // Courtesy of http://www.sitepoint.com/url-parameters-jquery/
-$.urlParam = function(name){
-    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-    if (results==null){
-       return null;
-    }
-    else{
-       return results[1] || 0;
-    }
+function getURIParam(name) {
+  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  if (results==null){
+    return null;
+  } else {
+      return results[1] || 0;
+  }
 }
 
-// Get the department param, check 'em
-var dept = $.urlParam('fos_fl');
-
-// Stubborn 2 letter departments
-if (dept.length == 1) {
+function getDeptFromURI(uri) {
+  // Get the department param, check 'em
+  var dept = getURIParam(uri);
+  // Stubborn 2 letter departments
+  if (dept.length == 1) {
   dept = dept + "++";
+  }
+  return dept;
 }
+
+dept = getDeptFromURI('fos_fl');
+
+
+
+
 
 // Figure out what the course number is
 // 
@@ -54,4 +64,28 @@ $("td.course_header h2").each(function() {
                  courseNumber + 
                  '&course_title=&unique=&instructor_first=&instructor_last=' + 
                  '&course_type=In+Residence&search=Search" target="_blank">View Syllabi and CVs</a>');
+});
+
+// Add link to CIS
+$('td[data-th="Instructor"]').each(function() {
+  var plainUriName = $(this).text().toLowerCase().capitalize();
+  if (plainUriName !== "") {
+    // Get the prof's name
+    var profName = plainUriName.split(", ");
+    // Get the prof's URI
+    var uriName = encodeURIComponent(plainUriName).replace(/%20/, '+');
+    // Change the name into a link
+    /*$(this).html('<a href="https://utdirect.utexas.edu/ctl/ecis/results' +
+                   '/search.WBX?s_in_search_type_sw=N&s_in_max_nbr_return=10&' +
+                   's_in_search_name=' +
+                   uriName +
+                   '" target="_blank">' +
+                   $(this).text() +
+                   '</a>');*/
+    $(this).append('<span>  (<a href="https://utdirect.utexas.edu/ctl/ecis/results' +
+                   '/search.WBX?s_in_search_type_sw=N&s_in_max_nbr_return=10&' +
+                   's_in_search_name=' +
+                   uriName +
+                   '" target="_blank">CIS</a>)</span>');
+  }
 });
